666f5b5bc04f7b3d829b69b7b647f4e3ea5c2f38
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-11-08 13:07:46 +0000

    NavBar done

---
+++ hist/src/pages/components/NavBar.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+const NavBar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [firstLoad, setFirstLoad] = useState(true);
+  const [NavVisible, setNavVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setNavVisible(prevScrollPos > currentScrollPos);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    setTimeout(() => setFirstLoad(false), 3000);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  const navbarStyle = {
+    transform: NavVisible ? "translateY(0)" : "translateY(-100%)",
+    transition: "transform 300ms",
+  };
+
+  return (
+    <nav
+      className={`z-20 fixed top-0 w-full ${
+        firstLoad ? "animate-fade-down" : "transition-all"
+      }`}
+      style={navbarStyle}
+    >
+      <ul className="flex flex-row py-4 justify-between items-center pl-6 pr-20 gap-x-3 md:gap-x-10 lg:gap-x-20 font-playfair text-xs md:text-lg lg:text-2xl">
+        <li>
+          <button
+            className="hover:text-accentColor transition-all scale-100 hover:scale-105"
+            onClick={(e) => {
+              setNavVisible(true);
+              document
+                .getElementById("home")
+                ?.scrollIntoView({ behavior: "smooth" });
+            }}
+          >
+            <Image
+              src="/hist.webp"
+              alt="Hist Logo"
+              width={150}
+              height={150}
+              className="rounded-xl"
+              priority
+              />              
+          </button>
+        </li>
+        <div className="flex flex-row justify-center gap-x-20">
+          <li>
+            <button
+              className="hover:text-accentColor transition-all scale-100 hover:scale-105"
+              onClick={(e) => {
+                document
+                  .getElementById("About Us")
+                  ?.scrollIntoView({ behavior: "smooth" });
+                setNavVisible(false);
+                setTimeout(() => {
+                  window.scrollTo(0, window.innerHeight * 2);
+                }, 300);
+              }}
+            >
+              About Us
+            </button>
+          </li>
+          <li>
+            <button
+              className="hover:text-accentColor transition-all scale-100 hover:scale-105"
+              onClick={(e) => {
+                setTimeout(() => {
+                  setNavVisible(false);
+                }, 1000);
+                document
+                  .getElementById("Be Our Providers")
+                  ?.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Be Our Providers
+            </button>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
--- hist/tailwind.config.ts
+++ hist/tailwind.config.ts
@@ -8,10 +8,95 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        textColor: "var(--text-color)",
+        backgroundColor: "var(--background-color)",
+        secondaryBackgroundColor: "var(--secondary-background-color)",
+        primaryColor: "var(--primary-color)",
+        secondaryColor: "var(--secondary-color)",
+        accentColor: "var(--accent-color)",
+      },
+      fontFamily: {
+        playfair: ["Playfair Display"],
+        lato: ["Lato"],
+      },
+      animation: {
+        "fade-in": "fade-in 3s ease-in-out forwards",
+        title: "title 3s ease-out forwards",
+        "fade-left": "fade-left 3s ease-in-out forwards",
+        "fade-right": "fade-right 3s ease-in-out forwards",
+        "fade-down": "fade-down 3s ease-in-out forwards",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0%",
+          },
+          "75%": {
+            opacity: "0%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        "fade-left": {
+          "0%": {
+            transform: "translateX(100%)",
+            opacity: "0%",
+          },
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        "fade-right": {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: "0%",
+          },
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        "fade-down": {
+          "0%": {
+            transform: "translateY(-200%)",
+            opacity: "0%",
+          },
+          "75%": {
+            opacity: "0%",
+          },
+          "100%": {
+            transform: "translateY(0%)",
+            opacity: "100%",
+          },
+        },
+        title: {
+          "0%": {
+            "line-height": "0%",
+            "letter-spacing": "0.25em",
+            opacity: "0",
+          },
+          "25%": {
+            "line-height": "0%",
+            opacity: "0%",
+          },
+          "80%": {
+            opacity: "100%",
+          },
+
+          "100%": {
+            "line-height": "100%",
+            opacity: "100%",
+          },
+        },
       },
     },
   },
