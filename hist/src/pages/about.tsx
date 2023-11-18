d6dccbabd12d9fa9a5ee3dfdc672d22981e627c3
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-11-18 09:08:14 +0000

    Added about page

---
+++ hist/src/pages/about.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head"
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
+
+const About = () => {
+    return (
+        <main>
+        <Head>
+            <title>Hist About</title>
+            <meta name="title" content="Hist" />
+            <meta name="description" content="Connecting Users with Providers" />
+            <meta name="keywords" content="hist healtcare booking" />
+            <meta name="author" content="Sean Chuah Tse Yung" />
+            <meta name="theme-color" content="#FFFFFF" />
+
+            <meta property="og:title" content="Hist" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="" />
+            <meta property="og:image" content="" />
+            <meta property="og:description" content="Connecting Users with Providers" />
+
+            <meta property="twitter:title" content="Hist" />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="" />
+            <meta property="twitter:image" content="" />
+            <meta property="twitter:description" content="Connecting Users with Providers" />
+        </Head>
+        <NavBar/>
+        <Footer/>
+    </main>
+    )
+}
